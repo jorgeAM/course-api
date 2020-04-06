7bbe7a6def5ba90b97b2004cd3f7dfd87fa30d21
Jorge L <jorge.alfmur@gmail.com>
2020-04-06 18:24:38 -0500

    setup database connection

---
+++ src/db/migrations/20200406232202-create-roles-table.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Roles', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('Roles')
+  }
+}
