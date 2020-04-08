d6b3d5965fe21ccd63183b9777e70dfcc9dd861b
Jorge L <jorge.alfmur@gmail.com>
2020-04-08 15:21:18 -0500

    serve static files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import cors from 'cors'
+import path from 'path'
 import express from 'express'
 import routes from './routes'
 
 const app = express()
 
+const publicFolder = path.resolve(__dirname, '../public')
+
+app.use(express.static(publicFolder))
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
